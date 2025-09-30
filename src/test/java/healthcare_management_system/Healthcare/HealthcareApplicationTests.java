201a67fab3f79adbd10985d6257f8fb93c6f68a4
gehad.ali <gehad.ali@jumia.com>
2025-10-01 01:28:29 +0300

    add inial project

---
+++ src/test/java/healthcare_management_system/Healthcare/HealthcareApplicationTests.java
@@ -0,0 +1,13 @@
+package healthcare_management_system.Healthcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
